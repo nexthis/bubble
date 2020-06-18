bbbc7c93551e358a87905002b6edb55f771a53ce
nexthis <45048369+nexthis@users.noreply.github.com>
2020-06-18 21:55:35 +0200

    page

--- example.ts
+++
@@ -1,5 +0,0 @@
-import { makeBubble, makeEventManager, EVENT_TYPE } from './src';
-
-const element = document.querySelector('.bubble') as HTMLElement;
-const eventManager = makeEventManager();
-const bubble = makeBubble({ element, eventManager });
